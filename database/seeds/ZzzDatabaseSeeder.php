f292ab8077ff5e1747241a237cd1b38658c8b3d8
menakafernando <project.menaka@gmail.com>
2020-03-08 11:47:30 +0200

    update

--- database/seeds/ZzzDatabaseSeeder.php
+++ database/seeds/ZzzDatabaseSeeder.php
@@ -21,12 +21,11 @@ class ZzzDatabaseSeeder extends Seeder
         //SELECT * FROM AccessChar INNER JOIN UserChar ON AccessChar.user_id=UserChar.id; // INNER JOIN
         //SELECT * FROM AccessChar LEFT JOIN UserChar ON AccessChar.user_id = UserChar.id; // LEFT JOIN
         //SELECT * FROM AccessChar RIGHT JOIN UserChar ON AccessChar.user_id = UserChar.id; // RIGHT JOIN
-        //SELECT * FROM AccessChar CROSS JOIN UserChar; //Cross JOIN
         //SELECT * FROM AccessChar LEFT JOIN UserChar ON AccessChar.phone = UserChar.phone WHERE UserChar.phone is NULL; // LEFT JOIN OUT
         //SELECT * FROM AccessChar RIGHT JOIN UserChar ON AccessChar.phone = UserChar.phone WHERE AccessChar.phone is NULL; // RIGHT JOIN OUT
         //SELECT * FROM AccessChar LEFT JOIN UserChar ON AccessChar.phone = UserChar.phone UNION SELECT * FROM AccessChar RIGHT JOIN UserChar ON AccessChar.phone = UserChar.phone; // FULL OUTER JOIN
         //SELECT * FROM AccessChar LEFT JOIN UserChar ON AccessChar.phone = UserChar.phone WHERE UserChar.phone is NULL UNION SELECT * FROM AccessChar RIGHT JOIN UserChar ON AccessChar.phone = UserChar.phone WHERE AccessChar.phone is NULL; // FULL OUTER JOIN OUT
-
+        //SELECT * FROM AccessChar CROSS JOIN UserChar; //Cross JOIN
 
         $this->call(ZzVarcharTableSeederCreate::class);
         $this->call(ZzVarcharTableSeederUpdate::class);
@@ -38,11 +37,12 @@ class ZzzDatabaseSeeder extends Seeder
         //SELECT * FROM AccessVarchar INNER JOIN UserVarchar ON AccessVarchar.user_id=UserVarchar.id; // INNER JOIN
         //SELECT * FROM AccessVarchar LEFT JOIN UserVarchar ON AccessVarchar.user_id = UserVarchar.id; // LEFT JOIN
         //SELECT * FROM AccessVarchar RIGHT JOIN UserVarchar ON AccessVarchar.user_id = UserVarchar.id; // RIGHT JOIN
-        //SELECT * FROM AccessVarchar CROSS JOIN UserVarchar; //Cross JOIN
         //SELECT * FROM AccessVarchar LEFT JOIN UserVarchar ON AccessVarchar.phone = UserVarchar.phone WHERE UserVarchar.phone is NULL; // LEFT JOIN OUT
         //SELECT * FROM AccessVarchar RIGHT JOIN UserVarchar ON AccessVarchar.phone = UserVarchar.phone WHERE AccessVarchar.phone is NULL; // RIGHT JOIN OUT
         //SELECT * FROM AccessVarchar LEFT JOIN UserVarchar ON AccessVarchar.phone = UserVarchar.phone UNION SELECT * FROM AccessVarchar RIGHT JOIN UserVarchar ON AccessVarchar.phone = UserVarchar.phone; // FULL OUTER JOIN
         //SELECT * FROM AccessVarchar LEFT JOIN UserVarchar ON AccessVarchar.phone = UserVarchar.phone WHERE UserVarchar.phone is NULL UNION SELECT * FROM AccessVarchar RIGHT JOIN UserVarchar ON AccessVarchar.phone = UserVarchar.phone WHERE AccessVarchar.phone is NULL; // FULL OUTER JOIN OUT
+        //SELECT * FROM AccessVarchar CROSS JOIN UserVarchar; //Cross JOIN
+
 
         $this->call(ZzIntTableSeederCreate::class);
         $this->call(ZzIntTableSeederUpdate::class);
@@ -54,11 +54,10 @@ class ZzzDatabaseSeeder extends Seeder
         //SELECT * FROM AccessInt INNER JOIN UserInt ON AccessInt.user_id=UserInt.id; // INNER JOIN
         //SELECT * FROM AccessInt LEFT JOIN UserInt ON AccessInt.user_id = UserInt.id; // LEFT JOIN
         //SELECT * FROM AccessInt RIGHT JOIN UserInt ON AccessInt.user_id = UserInt.id; // RIGHT JOIN
-        //SELECT * FROM AccessInt CROSS JOIN UserInt; //Cross JOIN
         //SELECT * FROM AccessInt LEFT JOIN UserInt ON AccessInt.phone = UserInt.phone WHERE UserInt.phone is NULL; // LEFT JOIN OUT
         //SELECT * FROM AccessInt RIGHT JOIN UserInt ON AccessInt.phone = UserInt.phone WHERE AccessInt.phone is NULL; // RIGHT JOIN OUT
         //SELECT * FROM AccessInt LEFT JOIN UserInt ON AccessInt.phone = UserInt.phone UNION SELECT * FROM AccessInt RIGHT JOIN UserInt ON AccessInt.phone = UserInt.phone; // FULL OUTER JOIN
         //SELECT * FROM AccessInt LEFT JOIN UserInt ON AccessInt.phone = UserInt.phone WHERE UserInt.phone is NULL UNION SELECT * FROM AccessInt RIGHT JOIN UserInt ON AccessInt.phone = UserInt.phone WHERE AccessInt.phone is NULL; // FULL OUTER JOIN OUT
-
+        //SELECT * FROM AccessInt CROSS JOIN UserInt; //Cross JOIN
     }
 }
